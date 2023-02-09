8b0f8ea4d1119868d65bb798bbf5643655ec7e78
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2023-02-09 12:11:54 -0300

    :wrench: fix font dimension and responsive structure

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,12 +2,13 @@ import { Heading, Stack, Text, useBreakpointValue, VStack } from '@chakra-ui/rea
 import React from 'react'
 
 function About() {
-  const fontSizeDinamic = useBreakpointValue({base:'xl', sm:'2xl'})
-  const lineHeightDinamic = useBreakpointValue({base:'9', sm:'1.4'})
-  const marginTopDinamic = useBreakpointValue({base:'20', sm:'32'})
+  const fontSizeDinamic = useBreakpointValue({base:'xl', sm:'2xl', lg:'3xl'})
+  const lineHeightDinamic = useBreakpointValue({base:'8', sm:'1.4'})
+  const marginTopDinamic = useBreakpointValue({base:'20', sm:'16', lg:'32'})
+  const marginXDinamic = useBreakpointValue({base:'5', sm:'22', lg:'52'})
   return (
-    <VStack w="full" h="100vh" p={5} alignItems="flex-start" id='About'>
-      <Stack p={5} mt={marginTopDinamic} bg={'rgba(255, 255, 255, 0.6)'} borderRadius={5} boxShadow='md'>
+    <VStack p={5} alignItems="flex-start" id='About'>
+      <Stack mb={'32'} mx={marginXDinamic} p={5} mt={marginTopDinamic} bg={'rgba(255, 255, 255, 0.6)'} borderRadius={5} boxShadow='md'>
         <Heading>About</Heading>
         <Text fontSize={fontSizeDinamic} lineHeight={lineHeightDinamic}>My name is Fernando, I am a front end web developer. Currently carrying out projects with back end tools for managing Sql and NoSql databases. Curious, empathetic and proactive. With experience working in groups, project management, repositories and eager to learn. Looking for my first job in the technology sector. 💻 💪</Text>
       </Stack>
